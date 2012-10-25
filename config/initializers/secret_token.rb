39bcb3b137dc29778e3e523b14a0d28680e288f6
Peter Blomgren <blomgren-p@mssu.edu>
2012-10-25 10:12:47 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookList::Application.config.secret_token = '7d0ffb92123fea8e40f91d12f7f523a5df3a8bdafc2d23bef46bacf9fc3f8f4ca32921a973dfe36322f5c0addc6b6c0256491341524aa27cd11c97826e43b114'
